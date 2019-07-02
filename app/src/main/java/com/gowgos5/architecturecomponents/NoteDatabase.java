67f3a6ec734d82348d93facb08d8c414a544e7c6
Vernon <gowgos5@gmail.com>
2019-07-02 17:24:50 +0800

    Introduction to Repository 1) Created a repository (an abstraction layer between RoomDatabase and ViewModel. 2) Understood static inner classes. 3) Implemented AsyncTask.

--- app/src/main/java/com/gowgos5/architecturecomponents/NoteDatabase.java
+++ app/src/main/java/com/gowgos5/architecturecomponents/NoteDatabase.java
@@ -1,28 +1,56 @@
 package com.gowgos5.architecturecomponents;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 // The @Database component combines the entities and the dao interfaces.
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
     // Singleton
     private static volatile NoteDatabase INSTANCE;
-
-    // abstract getter method for each @Dao
-    public abstract NoteDao noteDao();
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(INSTANCE).execute();
+        }
+    };
 
     public static synchronized NoteDatabase getInstance(Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
 
         return INSTANCE;
+    }
+
+    // Abstract getter method for each @Dao
+    // Room.databaseBuilder automatically generates the body for this abstract method
+    public abstract NoteDao noteDao();
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        public PopulateDbAsyncTask(NoteDatabase db) {
+            this.noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("title1", "desc1", 1));
+            noteDao.insert(new Note("title2", "desc2", 2));
+            noteDao.insert(new Note("title3", "desc3", 3));
+            return null;
+        }
     }
 }
